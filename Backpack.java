d517094b7e2a5d19e439d1cc6a55c6db05f07f03
ELee <elee.wingfield@gmail.com>
2018-05-01 06:15:38 +0100

    Update Backpack.java

--- Backpack.java
+++ Backpack.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 
 public class Backpack {
 	protected ArrayList<String> contents;
-	public double credits;
-	
+	public int credits;
+
 	public Backpack() {
 		contents = new ArrayList<String>();
+		contents.add("credits: 20");
+		contents.add("credits: 30");
+		contents.add("credits: 40");
 	}
-	
+
 	public void addCredits() {
-		
+		for (String item : contents) {
+			if (item.contains("credits: ")) {
+				String amount = item.substring(9);
+				int money =  Integer.parseInt(amount);
+				credits += money;
+			}
+		}
+		System.out.println(credits);
+	}
+	public static void main(String[] args) {
+		Backpack backpack = new Backpack();
+		backpack.addCredits();
 	}
 }
